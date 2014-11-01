ea7689be3b410ae135de4d28596d324e3460c061
Damian Dobrev <dmdbrv@gmail.com>
2014-11-01 12:35:29 +0000

    Grid ready

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -1,31 +1,65 @@
 var map;
+var currentLat;
+var currentLng;
+var initLat = 53.490009;
+var initLng = -2.261631;
+var squares = [];
+var squaresRow = [];
+var pos;
 
 function initialize() {
     var mapOptions = {
-        zoom: 8
+        zoom: 17
     };
     map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = new google.maps.LatLng(position.coords.latitude,
-                position.coords.longitude);
-
-            var infowindow = new google.maps.InfoWindow({
-                map: map,
-                position: pos,
-                content: 'Yo dawg, you are here.'
-            });
-
-            map.setCenter(pos);
-        }, function() {
-            handleNoGeolocation(true);
+    navigator.geolocation.getCurrentPosition(function(position) {
+        pos = new google.maps.LatLng(position.coords.latitude,
+            position.coords.longitude);
+        var infowindow = new google.maps.InfoWindow({
+            map: map,
+            position: pos,
+            content: 'Yo dawg, you are here.'
         });
-    } else {
-        // Browser doesn't support Geolocation
-        handleNoGeolocation(false);
+
+        map.setCenter(pos);
+    }, function() {
+        handleNoGeolocation(true);
+    });
+
+    for (var y = 0; y < 20; y++) {
+        for (var x = 0; x < 20; x++) {
+            squaresRow.push(new google.maps.Rectangle({
+                strokeColor: '#FF4400',
+                strokeOpacity: 0.4,
+                strokeWeight: 1,
+                fillColor: '#FF4400',
+                fillOpacity: 0.05,
+                map: map,
+                bounds: new google.maps.LatLngBounds(
+                    new google.maps.LatLng(initLat - y/1000, initLng + x / 500),
+                    new google.maps.LatLng(initLat - y/1000 - 0.001, initLng + 0.002 + x / 500))
+            }));
+        }
+        squares.push(squaresRow);
+        squaresRow = [];
     }
+    navigator.geolocation.getCurrentPosition(function(position) {
+        squares
+            [Math.floor(Math.abs(position.coords.latitude-initLat)*1000)]
+            [Math.floor(Math.abs(position.coords.longitude-initLng)*500)]
+            .fillOpacity=0.5;
+        console.log(Math.floor(Math.abs(position.coords.latitude-initLat)*1000));
+        console.log(Math.floor(Math.abs(position.coords.longitude-initLng)*500));
+    });
+}
+
+function getLat(lat) {
+    return Math.floor(Math.abs(lat-initLat)*1000);
+}
+function getLng(lng) {
+    return Math.floor(Math.abs(lng-initLng)*2000);
 }
 
 function handleNoGeolocation(errorFlag) {
